b56774eb623e0376d93678b84a7bb6d30c72d69f
Verybigfly <verybigfly.mine@gmail.com>
2022-01-21 21:05:03 +0100

    cette fois ça marche

--- install.sh
+++ install.sh
@@ -28,7 +28,6 @@ else
     echo -e 'charset utf-8;\ngeoip2 /data/geoip2/GeoLite2-City.mmdb {\n auto_reload 3h;\n $geoip2_metadata_country_build metadata build_epoch;\n $geoip2_data_country_code default=XX source=$remote_addr country iso_code;\n $geoip2_data_country_name default=- country names fr;\n $geoip2_data_city_name default=- city names fr;\n $geoip2_data_region_name default=- subdivisions 0 names fr;\n}\ngeo $allowed_ip {\n default no;\n 192.168.1.0/24 yes;\n}\n \nmap $geoip2_data_country_code $allowed_country {\n default $allowed_ip;\n FR yes;\n}\nlog_format proxy_geo escape=json '"'"'[$time_local] [Client $remote_addr] [$allowed_country $geoip2_data_country_code $geoip2_data_country_name $geoip2_data_region_name $geoip2_data_city_name] "$http_user_agent" '"'"'\n                                 '"'"'$upstream_cache_status $upstream_status $status - $request_method $scheme $host "$request_uri" [Length $body_bytes_sent] [Gzip $gzip_ratio] [Sent-to $server] "$http_referer"'"'"';' > ./data/data/nginx/custom/http_top.conf
     echo -e 'load_module /etc/nginx/modules/ngx_http_geoip2_module.so;\nload_module /etc/nginx/modules/ngx_stream_geoip2_module.so;' > ./data/modules/geoip2.conf
     echo -e 'if ($allowed_country = no) {\n	return 444;\n}' > ./data/data/nginx/custom/server_proxy.conf
-    docker exec -it nginxPM nginx -s reload
     echo "-------------------------------------------------"
     echo "-------------Installation Terminer---------------"
     echo "-------------------------------------------------"
